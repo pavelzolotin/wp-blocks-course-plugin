c81aaa5369224dccf469d9ca68f158381e046227
pavelzolotin <pavel.zolotin@goat.digital>
2022-04-06 21:50:58 +0500

    js and php filters

--- plugin.php
+++ plugin.php
@@ -12,9 +12,10 @@
  *
  */
 
-include_once('metabox.php');
-include_once('templates.php');
-include_once('patterns.php');
+include_once('php/metabox.php');
+include_once('php/templates.php');
+include_once('php/patterns.php');
+include_once('php/filters.php');
 
 function blocks_course_plugin_enqueue_assets() {
     $asset_file = include(plugin_dir_path(__FILE__) . 'build/index.asset.php');
